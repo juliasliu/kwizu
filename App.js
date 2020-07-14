98adb7ad0d8c99bb520a83c44a0c499ee93d520c
juliasliu <julia5sliu@gmail.com>
2020-07-13 23:26:18 -0700

    Test notifications worked

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 import useCachedResources from './hooks/useCachedResources';
 
@@ -9,20 +10,56 @@ import {users, chats, quizzes} from './stores';
 import Main from './screens/Main';
 
 export default function App(props) {
-  const isLoadingComplete = useCachedResources();
+	//Register background handler
+	messaging().setBackgroundMessageHandler(async remoteMessage => {
+	  console.log('Message handled in the background!', remoteMessage);
+	});
+	
+	useEffect(() => {
+		requestUserPermission();
+		const unsubscribe = messaging().onMessage(async remoteMessage => {
+			Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+		});
+//
+		return unsubscribe;
+	}, []);
+	
+	const isLoadingComplete = useCachedResources();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+				<View style={styles.container}>
 				{Platform.OS === 'ios' && <StatusBar barStyle="default" /> }
-				<Provider users={users} quizzes={quizzes} chats={chats}>
-					<Main/>
-				</Provider>
-			</View>
-    );
-  }
+					<Provider users={users} quizzes={quizzes} chats={chats}>
+						<Main/>
+					</Provider>
+				</View>
+		);
+	}
+}
+
+async function requestUserPermission() {
+	const authStatus = await messaging().requestPermission();
+	const enabled =
+		authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+		authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+	if (enabled) {
+		getFcmToken();
+		console.log('Authorization status:', authStatus);
+	}
+}
+
+getFcmToken = async () => {
+	const fcmToken = await messaging().getToken();
+	if (fcmToken) {
+		console.log(fcmToken);
+		console.log("Your Firebase Token is:", fcmToken);
+	} else {
+		console.log("Failed", "No token received");
+	}
 }
 
 const styles = StyleSheet.create({
