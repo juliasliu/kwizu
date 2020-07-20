99b05ef3eb00cd999d34a97cb699386a9d3d4d66
juliasliu <julia5sliu@gmail.com>
2020-07-19 19:14:44 -0700

    Updatet TestIds.INTERSTITIAL with production ad ID

--- screens/Take.js
+++ screens/Take.js
@@ -73,7 +73,7 @@ class Take extends React.Component {
 	showInterstitialAd = () => {
 		let id = "ca-app-pub-8298967838514788/3809240812";
 	    // Create a new instance
-	    const interstitialAd = InterstitialAd.createForAdRequest(TestIds.INTERSTITIAL);
+	    const interstitialAd = InterstitialAd.createForAdRequest(id);
 
 	    // Add event handlers
 	    interstitialAd.onAdEvent((type, error) => {
